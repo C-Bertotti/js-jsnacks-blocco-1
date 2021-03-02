f3bfe0974e4fa51d5551d540d5507d2a0e4f101d
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-02 11:15:31 +0100

    snack 1

---
+++ js-snack-1/js/script.js
@@ -0,0 +1,16 @@
+// L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore. 
+
+var userNumber1 = parseInt(prompt("inserisci un numero"));
+console.log("il numero che hai inserito è: " + userNumber1);
+
+var userNumber2 = parseInt(prompt("inserisci un numero"));
+console.log("il numero che hai inserito è: " + userNumber2);
+
+
+if ( userNumber1 > userNumber2 ) {
+    console.log("il numero maggiore che hai inserito è: " + userNumber1);
+} else if ( userNumber2 > userNumber1 ) {
+    console.log("il numero maggiore che hai inserito è: " + userNumber2);
+} else ( userNumber1 == userNumber2 ) {
+    console.log("i numeri che hai inserito sono uguali tra di loro");
+}
